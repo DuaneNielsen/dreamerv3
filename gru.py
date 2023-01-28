a7705605467b54d04180d3735739726b8df7cc6e
duanenielsen <duane.nielsen.rocks@gmail.com>
2023-01-28 12:47:37 -0800

    Added plotting.

--- gru.py
+++ gru.py
@@ -4,6 +4,7 @@ from torch.nn.functional import cross_entropy
 from torch.optim import Adam
 from replay import simple_trajectory, ReplayBuffer
 from env import Env
+from matplotlib import pyplot as plt
 
 if __name__ == '__main__':
 
@@ -26,14 +27,23 @@ if __name__ == '__main__':
         buffer += simple_trajectory([Env.right] * 8)
         buffer += simple_trajectory([Env.left])
 
+    # visualize
+    plt.ion()
+    fig, ax = plt.subplots(1)
+    scatter = ax.scatter([], [])
+    ax.set_xlim(0, 10)
+    ax.set_ylim(0, 10)
+    plt.draw()
+
     # train
     for epoch in range(1000):
         s, a, r, c, next_s, mask = buffer.sample_batch(L, N)
 
         output, _ = gru(s.flatten(-2))
         output = hidden_to_part(output).unflatten(-1, (C, D))
-        output = output.softmax(dim=2)
-        loss = cross_entropy(output.flatten(0, 1) * mask.reshape(N*L, 1, 1), next_s.flatten(0, 1))
+        output = output.softmax(2)
+        loss = cross_entropy(input=output.flatten(start_dim=0, end_dim=1) * mask.reshape(N * L, 1, 1),
+                             target=next_s.flatten(start_dim=0, end_dim=1))
         opt.zero_grad()
         loss.backward()
         opt.step()
@@ -50,5 +60,10 @@ if __name__ == '__main__':
                     if o - s_ > 1. and mask[l, n] == 1.:
                         transitions += [f"{s_} -> {o}"]
             print(transitions)
-
-
+            t0 = s.argmax(2).flatten()
+            t1 = output.argmax(2).flatten()
+            conn_matrix = torch.stack((t0, t1), dim=1)
+            conn_matrix = conn_matrix[mask.flatten() == 1.]
+            scatter.set_offsets(conn_matrix)
+            fig.canvas.draw()
+            fig.canvas.flush_events()
