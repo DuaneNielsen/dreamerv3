feb7c7248f6fe8977f63e8a163a97de2628d8e10
duanenielsen <duane.nielsen.rocks@gmail.com>
2023-02-24 23:41:21 -0800

    Buttonworld

---
+++ envs/buttonworld.py
@@ -0,0 +1,53 @@
+from __future__ import annotations
+
+from typing import SupportsFloat, Any, Tuple, Dict
+
+import cv2
+import gymnasium
+import numpy as np
+from gymnasium.core import RenderFrame, ActType, ObsType
+from gymnasium.spaces import MultiBinary, MultiDiscrete
+
+
+class ButtonWorld(gymnasium.Env):
+
+    def __init__(self, target, render_mode='human'):
+        super().__init__()
+        self.grid = None
+        self.target = target
+        self.observation_space = MultiBinary(self.target.shape)
+        self.action_space = MultiDiscrete(self.target.shape)
+        self.render_mode = render_mode
+        self.reset()
+
+    def render_human(self):
+        rgb_array = self.render()
+        cv2.imshow('buttonworld', rgb_array)
+        cv2.waitKey(1)
+
+    def reset(self, seed=None, **options):
+        self.grid = np.zeros(self.target.shape)
+        return self.grid
+
+    def step(self, action: ActType) -> Tuple[ObsType, SupportsFloat, bool, bool, Dict[str, Any]]:
+        self.grid[action[0], action[1]] = np.logical_not(self.grid[action[0], action[1]])
+        reward = np.logical_and(self.grid, self.target).sum()
+        terminated = np.allclose(self.grid, self.target)
+        if self.render_mode == 'human':
+            self.render_human()
+        return self.grid, reward, terminated, False, {}
+
+    def render(self) -> RenderFrame | list[RenderFrame] | None:
+        gray = self.grid.astype(np.uint8) * 255
+        gray = cv2.resize(gray, dsize=(300, 300))
+        return cv2.cvtColor(gray, cv2.COLOR_GRAY2RGB)
+
+
+if __name__ == '__main__':
+
+    target = np.ones((100, 100))
+    env = ButtonWorld(target)
+    obs = env.reset()
+    while True:
+        action = env.action_space.sample()
+        obs, reward, terminated, truncated, info = env.step(action)
